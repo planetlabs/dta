bcbedd9b9f4f3ba1b154d56084f3c696fd68c90b
Joseph Breu <breu@planet.com>
2015-09-29 16:03:09 +0000

    Updated for September 2015 release

--- extract-binaries.sh
+++ extract-binaries.sh
@@ -5,8 +5,6 @@ pushd binaries
 
 tail -n +$(awk '/^__START_OF_TARFILE__/ { print NR + 1; exit 0; }' ../DtapiService/DtapiService.bin) ../DtapiService/DtapiService.bin | tar zfxv -
 
-mv -f DtapiService32 DtapiServiced
-rm -f DtapiService64
 rm -f DtInfoCL
 
 wget -q http://www.dektec.com/Products/Apps/DtInfoCL/Downloads/DtInfoCL.zip
